f820675b6a97f600a6df2883a4dcd228d06796f9
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2021-09-22 09:42:50 +0200

    Add week 4 homeplay

---
+++ src/homeplay/week_4.R
@@ -0,0 +1,24 @@
+# Homeplay for week 4
+
+## Why are these questions suitable for collecting network data?
+
+### These questions are suitable for collecting network data, because they
+### capture the connections (edges) between students (vertices) in the class.
+### Additionally they capture the strength of the edges by asking how often
+### two students communicate.
+
+## Which question collects what kind of data?
+
+### The general questions in the first part of questionnaire capture vertex
+### attributes. The questions about who you interact with the most capture the
+### edges and the questions about how often you interact with that person and if
+### consider this person your friend capture edge attributes or edge strength.
+
+## What could be a bias for this data collection?
+
+### True level of friendship is hard to quantify. You have to select a fixed
+### number friends even though you might have relationships with more people.
+
+## What are the possible ethical concerns that might arise?
+
+### Asking questions about personal relationships could be morally questionable.
